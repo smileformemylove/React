39e5431ab79374f01a531142d87964a3c0a94607
Mykola <Shekezzz@gmail.com>
2025-01-13 21:03:14 +0200

    hw 53

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import MainTODO from "./components/MainTODO.jsx";
+import MainTODO from "./components/TodoApp.jsx";
 
 class App extends React.Component {
     render() {
